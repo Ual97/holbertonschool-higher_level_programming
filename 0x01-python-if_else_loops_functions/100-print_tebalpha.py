48457729595f1b7e9b21e4a70e614a64c458240c
Mateo Victorica <3951@holbertonschool.com>
2021-12-21 15:17:11 -0800

    removed 1 print

--- 0x01-python-if_else_loops_functions/100-print_tebalpha.py
+++ 0x01-python-if_else_loops_functions/100-print_tebalpha.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        print("{:c}".format(i - 32), end='')
-    if i % 2 == 0:
-        print("{:c}".format(i), end='')
+for a in range(122, 96, -1):
+    if a % 2 == 1:
+        a -= 32
+    print("{:c}".format(a), end="")
