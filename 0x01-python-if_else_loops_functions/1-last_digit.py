f6f8acc6452e8308219e59e1730e30d85727186a
Mateo Victorica <3951@holbertonschool.com>
2021-12-21 15:13:27 -0800

    fix error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-numpos = number * (-1)
+
 if number < 0:
-    if numpos % 10 == 0:
-        print("Last digit of {:d} is\
- {:d} and is 0".format(number, numpos % 10))
-    elif numpos % 10 > 5:
-        print("Last digit of {:d} is\
- {:d} and is greater than 5".format(number, numpos % 10))
-    elif numpos % 10 < 5:
-        print("Last digit of {:d} is {:d} and\
- is less than 6 and not 0".format(number, numpos % 10))
-elif number > 0:
-    if number % 10 == 0:
-        print("Last digit of {:d} is {:d}\
- and is 0".format(number, number % 10))
-    elif number % 10 > 5:
-        print("Last digit of {:d} is {:d} and\
- is greater than 5".format(number, number % 10))
-    elif number % 10 < 5:
-        print("Last digit of {:d} is {:d} and is\
- less than 6 and not 0".format(number, number % 10))
+    rem = number % (-10)
+else:
+    rem = number % 10
+
+if rem > 5:
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, rem))
+elif rem == 0:
+    print("Last digit of {} is {} and is 0"
+          .format(number, rem))
+else:
+    print("Last digit of {} is {} and is less than 6 and not 0"
+          .format(number, rem))
