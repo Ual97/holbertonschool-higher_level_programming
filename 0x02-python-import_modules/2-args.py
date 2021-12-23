966e2b9abfa0b3f6156a34b6998b55f82d025e50
Mateo Victorica <3951@holbertonschool.com>
2021-12-23 08:01:24 -0800

    2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    if  len(sys.argv) == 2:
+        print("1 argument:")
+        print("1: ", end='')
+        print(str(sys.argv[1]))
+    elif len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: ".format(i), end='')
+            print(str(sys.argv[i]))
+    else:
+        print("0 arguments.")
