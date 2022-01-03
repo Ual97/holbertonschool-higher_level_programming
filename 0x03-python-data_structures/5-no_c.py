a607fbd720464012f58fd51cee27ca7fde3dc0bd
Mateo Victorica <3951@holbertonschool.com>
2022-01-03 16:55:48 -0300

    no copy

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,6 +4,6 @@ def no_c(my_string):
     j = 0
     for i in range(len(my_string)):
         if (my_string[i] == 'c' or my_string[i] == 'C'):
-            new_string = new_string[:(i - j)] + my_string[(i + 1):]
+            new_string = new_string[:(i - j)] + new_string[(i + 1 - j):]
             j += 1
     return (new_string)
