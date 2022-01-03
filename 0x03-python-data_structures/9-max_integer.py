1fd57dadaed648847ebb24d8cfeeb77309d47c1d
Mateo Victorica <3951@holbertonschool.com>
2022-01-03 18:09:05 -0300

    max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    return (max(my_list))
+    if my_list is None:
+        return (None)
+    else:
+        answer = None
+        for i in my_list:
+            if i > answer:
+                answer = i
+        return (answer)
