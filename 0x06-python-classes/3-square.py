52c4738f82080a22a9c445a99c9d4a9ad9691904
Mateo Victorica <3951@holbertonschool.com>
2022-01-18 20:35:59 -0300

    added area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module 3-square
+defines class square with private attribute size
+"""
+
+
+from sunau import Au_read
+
+
+class Square:
+    """
+    class square definiton
+
+    Args:
+        size: size of square
+    """
+    def __init__(self, size=0):
+        """
+        initializes square
+
+        Attributes:
+            __size (int): size of square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        area of square
+
+        Returns:
+            area of square
+         """
+        return(self.__size)**2
