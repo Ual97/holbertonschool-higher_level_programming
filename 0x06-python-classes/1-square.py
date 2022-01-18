eae66baeed034789f5666a051340735b1f4f85a3
Mateo Victorica <3951@holbertonschool.com>
2022-01-18 16:28:04 -0300

    fixed coments

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+defines class square with private attribute size
+"""
+
+
+class Square:
+    """
+    class square definiton
+
+    Args:
+        size: size of square
+    """
+    def __init__(self, size):
+        """
+        initializes square
+
+        Attributes:
+            size: size of square
+        """
+        self.size = size
