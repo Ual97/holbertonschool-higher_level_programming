8d0c7160e3757f3bb9fa4cea98d791bcd44c5fbd
Mateo <3951@holbertonschool.com>
2022-01-24 18:39:49 -0300

    first push

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module-0-add_integer
+Takes 2 values and returns the sum of them, if a float value is passed it is converted to int
+"""
+
+def add_integer(a, b=98):
+    """
+    Returns sum of a + b as int, raises error if not int or float
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return(int(a) + int(b))
