e4b42cf1a1ab6f877604b9c90bdcad40eac40a21
Mateo Victorica <3951@holbertonschool.com>
2022-01-31 15:34:24 -0300

    added import from parent class BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle
+
+inherits from BaseGeometry
+instantiation of private attributes width and height
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """based on parent class BaseGeometry"""
+    def __init__(self, width, height):
+        """init with width and heigth"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
