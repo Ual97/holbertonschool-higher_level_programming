b6e9fdf2810b5673ef9e6b700b7ea7054e2dd09e
Mateo Victorica <3951@holbertonschool.com>
2022-01-31 23:03:06 -0300

    changed super for self

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 8-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validator
+"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Class that defines a rectangle from BaseGeometry Class """
-
+    """inherits from BaseGeometry
+    """
     def __init__(self, width, height):
-        """ Initializes instance """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        """validate and initialize width and height
+        """
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
