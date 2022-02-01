d0cd8ae0c90e1a07caa276280c316d6fecba8e19
Mateo <3951@holbertonschool.com>
2022-02-01 10:37:24 -0800

    fixed args

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 arg_list = sys.argv
-print(arg_list)
-# save_to_json_file(new_list, "add_item.json")
-# load_from_json_file("add_item.json")
+new_list = (arg_list[1:])
+save_to_json_file(new_list, "add_item.json")
+load_from_json_file("add_item.json")
+
