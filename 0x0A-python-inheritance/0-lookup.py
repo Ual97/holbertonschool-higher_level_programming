6a5b812805a2929cdae40dc403244663755100cd
Mateo Victorica <3951@holbertonschool.com>
2022-01-31 23:10:51 -0300

    added more comments

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -9,5 +9,10 @@ contains methos that returns objects attributes and methods
 def lookup(obj):
     """
     returns all methods and attributes
+
+    Args:
+        obj: instance of class
+    Returns:
+        List of attributes and methods
     """
     return dir(obj)
