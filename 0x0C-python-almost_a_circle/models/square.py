f255cff66ad2598cd7699fc4ddacc1f0832a8e50
Mateo Victorica <3951@holbertonschool.com>
2022-02-07 15:30:30 -0300

    added square class with __str__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,9 +21,8 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
         self.size = size
 
-
-def __str__(self):
-    """returns [Square] (<id>) <x>/<y> - <size>"""
-    return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
+    def __str__(self):
+        """returns [Square] (<id>) <x>/<y> - <size>"""
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}".format(
             self.__class__.__name__, self.id, self.x, self.y,
             self.size)
