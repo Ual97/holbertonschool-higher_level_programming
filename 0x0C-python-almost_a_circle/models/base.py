71cd385b41a6208c10215c44845a9aeb5e431143
Mateo Victorica <3951@holbertonschool.com>
2022-02-07 22:41:22 -0300

    added JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,9 @@
 Base class, with private class attribute __nb_objects = 0
 and class constructor __init__
 """
+from fileinput import filename
+import json
+import csv
 
 
 class Base():
@@ -21,3 +24,20 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """JSON representation of a string"""
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
+
+    def save_to_file(cls, list_objs):
+        """writes JSON representation of list_objs to a file"""
+        obj = []
+        if list_objs is not None:
+            for i in list_objs:
+                obj.append(cls.to_dictionary(i))
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            f.write(cls.to_json_string(obj))
