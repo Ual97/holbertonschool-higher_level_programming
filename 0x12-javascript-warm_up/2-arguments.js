fa8c2184c3b5d0f559e7c199149797d3af9cc1c4
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 14:06:11 -0300

    2plz

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,8 +3,8 @@
 const var1 = 'No argument';
 const var2 = 'Argument found';
 const var3 = 'Arguments found';
-if (process.argv) {
-  console.log(var1 === 0);
+if (process.argv === 0) {
+  console.log(var1);
 } else if (process.argv === 1) {
   console.log(var2);
 } else if (process.argv > 1) {
