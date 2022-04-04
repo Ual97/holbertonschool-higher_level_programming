67cebfae3fe97dcf78527bda77d16dd94cfe299f
Mateo <3951@holbertonschool.com>
2022-04-04 13:19:45 -0700

    fixed finally 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-let i = 0;
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const arr = process.argv;
-  const sortedarr = arr.sort(function(a, b){return a - b});
+  const sortedarr = arr.sort(function (a, b) { return a - b; });
   const arrlen = sortedarr.length;
   console.log(sortedarr[arrlen - 2]);
 }
