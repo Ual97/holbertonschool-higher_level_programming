e51b25d93d7ebe0b796d0cd7ce7622a913231413
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 16:11:50 -0300

    ex9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const var1 = parseInt(process.argv[2]);
+const var2 = parseInt(process.argv[3]);
+if (Number.isInteger(var1) && Number.isInteger(var2)) {
+  console.log(var1 + var2);
+}
