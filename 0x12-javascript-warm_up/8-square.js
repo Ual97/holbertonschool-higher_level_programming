650dcfcb9da8988259581a44342488e213cf660f
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 16:05:32 -0300

    ex8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,8 @@ const var1 = parseInt(process.argv[2]);
 const var2 = 'X'
 if (Number.isInteger(var1)) {
   for (let i = 0; i < var1; i++) {
-    var2.repeat(var1);
+    console.log(var2.repeat(var1));
   }
+} else {
+  console.log('Missing size');
 }
