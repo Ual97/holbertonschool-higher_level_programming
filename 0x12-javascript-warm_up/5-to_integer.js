d67bc01727d6f5f1379979c8fc883c16e0291812
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 15:22:20 -0300

    5-to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const var1 = (process.argv[2]);
-if (Number.isInteger(var1)) {
+if (Number.isInteger(var1) === true) {
   console.log('My number: ' + parseInt(var1));
 } else {
   console.log('Not a number');
