e461163c56743640146571e899644fb06e6bb9c0
Mateo <3951@holbertonschool.com>
2022-04-04 12:15:43 -0700

    fixed indents 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,5 @@ const var2 = parseInt(process.argv[3]);
 if (Number.isInteger(var1) && Number.isInteger(var2)) {
   console.log(var1 + var2);
 } else {
-    console.log('NaN');
+  console.log('NaN');
 }
