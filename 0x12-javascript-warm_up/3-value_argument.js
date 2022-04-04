f1213af89b23a803804ae6331129713364d57ea6
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 14:31:56 -0300

    ex3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 
 const var1 = 'No argument';
+const var2 = process.argv[3];
+console.log(var2);
 if (process.argv.length <= 2) {
   console.log(var1);
 } else {
-  console.log(process.argv[4])
+  console.log(process.argv[3])
 }
