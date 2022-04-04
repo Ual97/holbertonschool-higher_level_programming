d8c44e5046ac90815fc08205dad34a4014bf94ea
Mateo <3951@holbertonschool.com>
2022-04-04 11:38:33 -0700

    fixed 6 indents

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const var1 = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const var1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
-    console.log(var1[i]);
+  console.log(var1[i]);
 }
