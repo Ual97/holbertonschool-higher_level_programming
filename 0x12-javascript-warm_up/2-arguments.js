9abeb5b1363abacdef7f7e5acfc20b2776e48206
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 14:02:02 -0300

    added === 0 to first case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const var1 = 'No argument';
 const var2 = 'Argument found';
 const var3 = 'Arguments found';
 if (process.argv) {
-  console.log(var1);
+  console.log(var1 === 0);
 } else if (process.argv === 1) {
   console.log(var2);
 } else if (process.argv > 1) {
