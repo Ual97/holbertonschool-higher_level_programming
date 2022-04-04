8f3c574711b401ec1a27ae93a0d0484a60117dcd
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 16:25:00 -0300

    ex10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const var1 = parseInt(process.argv[2]);
 function factorial(var1) {
-  if (var1 === 0) {
-    console.log('NaN');
+  if (!var1) {
+    console.log('1');
   } else {
     console.log(var1 * factorial(var1 - 1));
   }
