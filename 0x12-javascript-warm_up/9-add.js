9eb1f4a1b9e1820cfd3d7ad227d6c11a05765583
Mateo <3951@holbertonschool.com>
2022-04-04 12:14:06 -0700

    added NaN

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,4 +4,6 @@ const var1 = parseInt(process.argv[2]);
 const var2 = parseInt(process.argv[3]);
 if (Number.isInteger(var1) && Number.isInteger(var2)) {
   console.log(var1 + var2);
+} else {
+    console.log('NaN');
 }
