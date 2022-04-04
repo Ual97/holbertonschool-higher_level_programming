5532cf708e2f7be47345488c07c54c6cc4b41239
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 15:50:04 -0300

    7ex

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 const var1 = 'C is fun';
-const var2 = parseInt(var1);
-if (Number.isInteger(parseInt(var1))) {
+const var2 = parseInt(process.argv[2]);
+if (Number.isInteger(var2)) {
   for (let i = 0; i < var2; i++) {
     console.log(var1);
   }
