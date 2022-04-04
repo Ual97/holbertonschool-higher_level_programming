b2676eff23c69f86aeb13d6ab2d4e629db6a72c6
Mateo <3951@holbertonschool.com>
2022-04-04 12:07:22 -0700

    chmod 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const var1 = parseInt(process.argv[2]);
-const var2 = 'X'
+const var2 = 'X';
 if (Number.isInteger(var1)) {
   for (let i = 0; i < var1; i++) {
     console.log(var2.repeat(var1));
