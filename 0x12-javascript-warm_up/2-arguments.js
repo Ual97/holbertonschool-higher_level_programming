ae94abaf8225e5a35c982a5cddab89e1f0727c72
Mateo Victorica <3951@holbertonschool.com>
2022-04-04 14:12:30 -0300

    added length

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,7 @@
 const var1 = 'No argument';
 const var2 = 'Argument found';
 const var3 = 'Arguments found';
+console.log(process.argv.length);
 if (process.argv.length === 0) {
   console.log(var1);
 } else if (process.argv.length === 1) {
