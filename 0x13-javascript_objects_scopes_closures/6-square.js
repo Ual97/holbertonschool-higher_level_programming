bce42bb97fc8fa032b5a2fa9c115d18d85bd0ed0
Mateo <3951@holbertonschool.com>
2022-04-05 13:09:13 -0700

    added uppercase C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -14,7 +14,7 @@ module.exports = class Square extends Rectangle {
       }
     } else {
       for (let i = 0; i < this.width; i++) {
-        console.log('c'.repeat(this.height));
+        console.log('C'.repeat(this.height));
       }
     }
   }
