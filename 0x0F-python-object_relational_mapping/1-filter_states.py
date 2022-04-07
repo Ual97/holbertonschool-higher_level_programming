ec147fb97c082330aad2ecb438cb9b3e30456b27
Mateo <3951@holbertonschool.com>
2022-04-07 10:00:28 -0700

    ex 0 and 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""script that lists all states from databse hbtn_0e_0_usa"""
+
+"""script that lists all states with a name starting with N from the databse hbtn_0e_0_usa."""
 
 import MySQLdb
 from sys import argv
+
 db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY id ASC")
+cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 query_row = cursor.fetchall()
 for row in query_row:
     print (row)
