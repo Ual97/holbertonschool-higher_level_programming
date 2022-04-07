5033f0e85efb67bc96310cf37d15ddbddb46cd60
Mateo <3951@holbertonschool.com>
2022-04-07 11:11:16 -0700

    exes 3 y 4

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,8 @@ from sys import argv
 
 db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
+query = ("SELECT * FROM states WHERE name=%s ORDER BY id ASC")
+cursor.execute(query, (argv[4],))
 query_row = cursor.fetchall()
 for row in query_row:
     print (row)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,7 +7,7 @@ from sys import argv
 
 db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
+cursor.execute("SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC")
 query_row = cursor.fetchall()
 for row in query_row:
     print (row)
