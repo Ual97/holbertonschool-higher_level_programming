d22a0bd5653b0827b7d5a592ed73f6442dfbd136
Mateo <3951@holbertonschool.com>
2022-04-08 13:15:43 -0700

    fixed pycode ex7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""script that lists all states with a name starting with N from the databse hbtn_0e_0_usa."""
+"""script that lists all states with a name starting
+ with N from the databse hbtn_0e_0_usa."""
 
 import MySQLdb
 from sys import argv
@@ -13,7 +14,7 @@ passwd = argv[2]
 db = argv[3]
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                        format(user, passwd, db), pool_pre_ping=True)
+                       format(user, passwd, db), pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
