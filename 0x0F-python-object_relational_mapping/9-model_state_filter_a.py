983f4a378d24c3f8fb526296f75ec3026598b02a
Mateo <3951@holbertonschool.com>
2022-04-10 12:33:47 -0700

    pep 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -9,11 +9,12 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-user = argv[1]
-passwd = argv[2]
-db = argv[3]
 
 if __name__ == "__main__":
+
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(user, passwd, db), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
