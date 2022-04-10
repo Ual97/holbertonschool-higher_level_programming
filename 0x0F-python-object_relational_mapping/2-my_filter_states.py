cb310d048a67af3505e876896ba5c20a79c05913
Mateo <3951@holbertonschool.com>
2022-04-10 11:59:43 -0700

    fixed pep8 ex2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 
-"""script that lists all states with a name starting with N from the databse hbtn_0e_0_usa."""
+"""script that lists all values in states
+ where name matches the argument"""
 
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
-query_row = cursor.fetchall()
-for row in query_row:
-    print (row)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                   (argv[4],))
+    query_row = cursor.fetchall()
+    for row in query_row:
+        print(row)
+    cursor.close()
+    db.close()
