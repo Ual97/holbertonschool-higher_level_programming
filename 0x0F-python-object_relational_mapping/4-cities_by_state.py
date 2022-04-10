1ed8e482cc987d0d5d26a968da14917adb96c374
Mateo <3951@holbertonschool.com>
2022-04-10 12:13:54 -0700

    fixed pep ex4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 
-"""script that lists all states with a name starting with N from the databse hbtn_0e_0_usa."""
+"""script that lists all states from databse hbtn_0e_0_usa."""
 
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cursor = db.cursor()
-cursor.execute("SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC")
-query_row = cursor.fetchall()
-for row in query_row:
-    print (row)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    qry = """SELECT cities.id, cities.name, states.name
+             FROM states INNER JOIN cities
+             ON states.id = cities.state_id ORDER BY cities.id ASC"""
+    cursor.execute(qry)
+    query_row = cursor.fetchall()
+    for row in query_row:
+        print(row)
+    cursor.close()
+    db.close()
