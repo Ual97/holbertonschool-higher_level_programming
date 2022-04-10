c41e640cc448147a1bda8fdb3f4c88faeb0f5498
Mateo <3951@holbertonschool.com>
2022-04-10 14:46:46 -0700

    ex 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""script that lists all states with a name starting
+ with N from the databse hbtn_0e_0_usa."""
+
+from itertools import count
+import MySQLdb
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new = State(name="Louisiana")
+    session.add(new)
+    session.commit()
+    print("{:d}".format(new.id))
+    session.close()
