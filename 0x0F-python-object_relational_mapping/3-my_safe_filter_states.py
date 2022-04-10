15ef1892e218b06dfe3e8e36bbdb241f6b2b2263
Mateo <3951@holbertonschool.com>
2022-04-10 12:10:19 -0700

    fixed pepex3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 
-"""script that lists all states with a name starting with N from the databse hbtn_0e_0_usa."""
+"""script that makes SQL protected query"""
 
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-cursor = db.cursor()
-query = ("SELECT * FROM states WHERE name=%s ORDER BY id ASC")
-cursor.execute(query, (argv[4],))
-query_row = cursor.fetchall()
-for row in query_row:
-    print (row)
-cursor.close()
-db.close()
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    query = ("SELECT * FROM states WHERE name=%s ORDER BY id ASC")
+    cursor.execute(query, (argv[4],))
+    query_row = cursor.fetchall()
+    for row in query_row:
+        print(row)
+    cursor.close()
+    db.close()
