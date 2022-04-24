c37cf15b3dc9efd63a6bfb40c27beee736650d6b
Mateo Victorica <3951@holbertonschool.com>
2022-04-24 16:34:26 -0300

    ex 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""python scrpit that displays value of X-Request-Id of a given url"""
+
+from sys import argv
+import urllib.request
+import urllib.response
+
+with urllib.request.urlopen(argv[1]) as response:
+    html = response.read()
+    print(html.headers)
